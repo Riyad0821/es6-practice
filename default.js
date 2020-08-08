5f4b36ecd21da593748c8e590125321b5e608d9c
Riyad0821 <mdriyad0821@gmail.com>
2020-08-09 00:45:10 +0600

    complete template practice

--- default.js
+++ default.js
@@ -1,6 +1,6 @@
-function add(num1, num2){
-    num2 = num2 || 0;
+function add(num1, num2 = 1){//backup value
+    
     return num1 + num2;
 }
-const total = add(15);
+const total = add(15, 20);
 console.log(total);
---
+++ template.js
@@ -0,0 +1,15 @@
+const firstName = "Justin";
+const lastName = "TimberLake";
+const fullName = firstName +" "+ lastName + " is a good boy.";
+const fullName2 = `${firstName} ${lastName} ${20+20} is a good boy.`;
+const multiLine = "I love You\n"
++"I miss you\n"
++"I need you";
+const multiLine2 = `I love you
+I miss you
+no. I don't need you
+Baily road a dorkar nai.`;
+console.log(fullName);
+console.log(fullName2);
+console.log(multiLine);
+console.log(multiLine2);
