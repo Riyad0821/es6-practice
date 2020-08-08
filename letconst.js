7a2c29c6052ead14d741f776717ce20de3f61c06
Riyad0821 <mdriyad0821@gmail.com>
2020-08-09 00:16:59 +0600

    practice done

---
+++ letconst.js
@@ -0,0 +1,17 @@
+const friend = "Shahed";
+//friend = "Mahmud"; //cannot possible
+console.log(friend);
+const numbers = [12, 45];
+numbers[1] = 88;
+numbers.push(12);
+console.log(numbers);
+
+const nayok = {name: "sakib khan", phone: 4578};
+let patientName = "A.Rahim Chacha";
+patientName = "Fatema khala";
+console.log(patientName);
+let sum = 0;
+for(let i = 0; i < 10; i++){
+    sum = sum + i;
+}
+console.log(sum);
